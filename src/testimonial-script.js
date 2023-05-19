0b256a72f4f2d246242f73ffd57237dac3bef1b2
jophc1 <jphcairns@gmail.com>
2023-05-19 16:23:41 +1000

    changes to index.html

---
+++ src/testimonial-script.js
@@ -0,0 +1,36 @@
+(function () {
+	'use strict';
+	// this function is strict
+}());
+(function () {
+   // but this function is sloppy
+}());
+
+const catherine = document.getElementById("catherine");
+const tilly = document.getElementById("tilly");
+const hugh = document.getElementById("hugh");
+const ryan = document.getElementById("ryan");
+
+function myTimerFive() {
+    catherine.style.display = "block";
+
+}
+
+function myTimerten() {
+    catherine.style.display = "none";
+}
+
+
+function cycleText() {
+    
+}
+
+function cycleTextOff() {
+   
+}
+
+
+for (let i = 0; i < 3; i++) {
+        catherine.style.display = "none";
+}
+
