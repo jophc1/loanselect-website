7e07c7f07a88e0579bf521cdd46695a316d8065a
jophc1 <jphcairns@gmail.com>
2023-06-02 14:21:06 +1000

    added emailjs service to contact form

---
+++ src/emailjs.js
@@ -0,0 +1,21 @@
+
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('Qli_SeLXTcrpI64tl');
+})();
+
+window.onload = function() {
+    document.getElementById("contact-form").reset();
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        // these IDs from the previous steps
+        emailjs.sendForm('service_rwf6ign', 'template_iizwdpx', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    document.getElementById("contact-form").reset();
+    });
+}
